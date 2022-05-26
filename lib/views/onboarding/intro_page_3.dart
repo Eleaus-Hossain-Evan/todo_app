1fc8944ccd76a27d5d785e64d7b81ef62f0c9530
dev-squadhead <squadhead247@gmail.com>
2022-05-27 01:48:57 +0600

    onboarding functionality implemented

---
+++ lib/views/onboarding/intro_page_3.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class IntroPage3 extends StatelessWidget {
+  const IntroPage3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Page 3"),
+      ),
+    );
+  }
+}
---
+++ lib/views/onboarding/onboarding_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:todo_app/views/home/home_page.dart';
+import 'package:todo_app/views/onboarding/intro_page_1.dart';
+import 'package:todo_app/views/onboarding/intro_page_2.dart';
+import 'package:todo_app/views/onboarding/intro_page_3.dart';
+
+class OnboardingPage extends StatelessWidget {
+  OnboardingPage({Key? key}) : super(key: key);
+
+  final PageController _pageController = PageController();
+
+  RxBool isLastPage = false.obs;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Obx(
+        () => Stack(
+          children: [
+            PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                isLastPage(index == 2);
+              },
+              children: const [
+                IntroPage1(),
+                IntroPage2(),
+                IntroPage3(),
+              ],
+            ),
+            Container(
+              alignment: const Alignment(0, .75),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  InkWell(
+                    onTap: () => _pageController.jumpToPage(2),
+                    child: const Text("Skip"),
+                  ),
+                  SmoothPageIndicator(
+                    controller: _pageController,
+                    count: 3,
+                  ),
+                  isLastPage.isTrue
+                      ? InkWell(
+                          onTap: () => Get.to(const HomePage()),
+                          child: const Text("Done"),
+                        )
+                      : InkWell(
+                          onTap: () => _pageController.nextPage(
+                            duration: const Duration(milliseconds: 500),
+                            curve: Curves.easeIn,
+                          ),
+                          child: const Text("Next"),
+                        ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
